b7c248a03cd5d0d41ea1dd2b33a47199d0994c86
ofekraf <ofekraf+github@gmail.com>
2019-03-02 22:25:04 +0200

    added login blueprint

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -3,6 +3,10 @@ from flask import (Blueprint, flash, g, redirect, render_template, request, sess
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
+CHECK_EXISTING_USERNAME_SQL_QUERY = 'SELECT * FROM user WHERE username = ?'
+
+AUTH_PREEFIX = '/auth'
+BLUEPRINT_NAME = 'auth'
 
 #SQL COMMANDS
 INSERT_NEW_USER_SQL_COMMAND = 'INSERT INTO user (username, password) VALUES (?, ?)'
@@ -12,9 +16,9 @@ CHECK_EXISTING_USERNAME_SQL_QUERY = 'SELECT id FROM user WHERE username = ?'
 MISSING_PASSWORD_ERROR = 'Password is required.'
 EXISTING_USERNAME_ERROR = 'User {} is already registered.'
 MISSING_USERNAME_ERROR = 'Username is required.'
+INCORRECT_PASSWORD_ERROR = 'Incorrect password.'
+INCORRECT_USERNAME_ERROR = 'Incorrect username.'
 
-AUTH_PREEFIX = '/auth'
-BLUEPRINT_NAME = 'auth'
 
 bp = Blueprint(BLUEPRINT_NAME, __name__, url_prefix=AUTH_PREEFIX)
 
@@ -33,40 +37,42 @@ def register():
     return render_template('auth/register.html') #todo - change to constant?
 
 
-def check_error_user_registration(db, password, username):
-    """ :return: an error is such occurred. None otherwise    """
-    error = None
-    if not username:
-        error = MISSING_USERNAME_ERROR
-    elif not password:
-        error = MISSING_PASSWORD_ERROR
-    elif db.execute(CHECK_EXISTING_USERNAME_SQL_QUERY,
-                    (username,)).fetchone() is not None:
-        error = EXISTING_USERNAME_ERROR.format(username)
-    return error
-
-
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         db = get_db()
-        error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
-
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
-
+        error, user = check_error_user_login(db, password, username)
         if error is None:
             session.clear()
             session['user_id'] = user['id']
             return redirect(url_for('index'))
-
         flash(error)
-
     return render_template('auth/login.html')
+
+
+
+
+def check_error_user_registration(db, password, username):
+    """ :return: an error message if such occurred. None otherwise    """
+    error = None
+    if not username:
+        error = MISSING_USERNAME_ERROR
+    elif not password:
+        error = MISSING_PASSWORD_ERROR
+    elif db.execute(CHECK_EXISTING_USERNAME_SQL_QUERY,(username,)).fetchone() is not None:
+        error = EXISTING_USERNAME_ERROR.format(username)
+    return error
+
+
+def check_error_user_login(db, password, username):
+    """ :return: an error message if such occurred. None otherwise    """
+    error = None
+    user = db.execute(CHECK_EXISTING_USERNAME_SQL_QUERY,
+                      (username,)).fetchone()
+    if user is None:
+        error = INCORRECT_USERNAME_ERROR
+    elif not check_password_hash(user['password'], password):
+        error = INCORRECT_PASSWORD_ERROR
+    return error, user
